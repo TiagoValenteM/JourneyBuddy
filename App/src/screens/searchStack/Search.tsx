990f156abd287241c90466dee036f33017197e44
Vinicius Lallo Costa Pinto <83062801+vilallo@users.noreply.github.com>
2023-06-16 08:12:19 +0100

    Update Search.tsx
    Search Updated - Some Logic, not functional yet

--- App/src/screens/searchStack/Search.tsx
+++ App/src/screens/searchStack/Search.tsx
@@ -1,22 +1,92 @@
-import React from "react";
-import { Text, View } from "react-native";
+import React, { useState } from "react";
+import { Text, View, ScrollView, RefreshControl, TouchableOpacity, StyleSheet } from "react-native";
 import { useAuth } from "../../hooks/useAuth";
+import SearchBar from "../../components/searchbar/SearchBar";
+import { Feather } from '@expo/vector-icons';
+
 
 function SearchView<StackScreenProps>({ navigation }: { navigation: any }) {
   const { user } = useAuth();
+  const [searchForGuides, setSearchForGuides] = useState(true);
+  //This useState will be True if I'm searching for guides and will be set to
+  //false if I'm searching for People. 
+  //Since we only have 2 options(Guides or People) is easier to do it in this way.
+  const handleSearch = (query: string) => {
+    console.log(`Searching for ${searchForGuides ? 'guides' : 'people'} - Query: ${query}`);
 
+      // Perform search logic based on searchForGuides and query
+  if (searchForGuides) {
+    // Perform guide search logic
+    // You can use a guide-specific API or query here
+    // For example: searchGuides(query)
+  } else {
+    // Perform people search logic
+    // You can use a people-specific API or query here
+    // For example: searchPeople(query)
+  }
+};
   return (
-    <View
-      style={{
-        width: "100%",
-        height: "100%",
-        alignItems: "center",
-        justifyContent: "center",
-      }}
+    <ScrollView
+      style={{ flex: 1, paddingHorizontal: 20 }}
+    /*refreshControl={
+      <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+    }*/
     >
-      <Text>Missing something here ;(</Text>
-    </View>
+      <View>
+        {/*<SearchBar onSearch={handleSearch}/>*/}
+        <SearchBar onSearch={handleSearch} searchForGuides={searchForGuides} toggleSearchType={() => setSearchForGuides(!searchForGuides)} />
+
+      </View>
+
+      <View style={styles.GuideContainer}>
+        <TouchableOpacity style={searchForGuides ? styles.selectedChoice : styles.choice} /*onPress={toggleSearchType}*/>
+          <Feather name="user" size={20} />
+          <Text style={styles.TitleChoice}>Guides</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={!searchForGuides ? styles.selectedChoice : styles.choice} /*onPress={toggleSearchType}*/>
+          <Feather name="user" size={20} />
+          <Text style={styles.TitleChoice}>People</Text>
+        </TouchableOpacity>
+
+      </View>
+
+    </ScrollView>
+
+
   );
 }
 
+const styles = StyleSheet.create({
+  GuideContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: "space-between",
+    backgroundColor: '#d3d3d3',
+    paddingEnd: 70,
+    paddingStart: 70,
+    paddingHorizontal: 10,
+    borderRadius: 8,
+    marginHorizontal: 10,
+    marginTop: 15,
+    marginBottom: 5,
+  },
+  TitleChoice: {
+    fontSize: 20,
+  },
+  //Title theme
+  choice: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    // Add your choice styles here
+  },
+
+  selectedChoice: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    // Add your selected choice styles here
+  },
+})
+
 export default SearchView;
+
